3f3559d7153158fa9cc53eeb30878a2c92ba64cf
Yevhen Surfer <23skopets@gmail.com>
2024-03-18 14:16:52 +0100

    test project module 3.2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
 bool found = false; // Initialize found outside the loop
-
 int total = 0; 
+
 foreach (int number in numbers)
 {
     total += number;
-
     if (number == 42)
     {
         found = true;
